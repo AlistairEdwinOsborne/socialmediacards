5653b6410ff090e57d9caf0e026d98e8797ada7f
AlistairEdwinOsborne <alibob.osbat@googlemail.com>
2018-09-19 13:21:57 +0100

    added linkify lib

--- js/main.js
+++ js/main.js
@@ -11,22 +11,17 @@ $.getJSON('http://private-cc77e-aff.apiary-mock.com/posts', function (data) {
         if(postObj[i].service_name == "Manual"){
            manualCard(postObj[i]);
         } else if(postObj[i].service_name == "Twitter"){
-
-             twitterCard(postObj[i]);   
-            
-        } 
-        
-        
-    }
-    
+            twitterCard(postObj[i]);     
+        } else if(postObj[i].service_name == "Instagram"){
+            instagramCard(postObj[i]);
+        }       
+    } 
 });
 
 
-$(document).ready(function () {
-
-    
-    
+$(document).ready(function () { 
 });
+
 
 /* Data from manual card
 item_data.text
@@ -36,10 +31,13 @@ item_data.image_url
 service_name
 */
 var manualCard = function(Object){
-    var myCol = $('<div class="col-lg-4 col-md-16 col-sm-6 col-xs-6 align-items-stretch mt-4"></div>');
+    var myCol = $('<div class="col-lg-4 col-md-16 col-sm-6 col-xs-6 align-items-stretch mt-4 manual"></div>');
         var myPanel = $('<div class="card card-outline-info" id="'+Object.item_id+'Panel"><div class="card-block"><img class="card-img-top" src="images/ali.jpg" alt="Card image cap"><div class="card-body"><p class="card-text">'+Object.item_data.text+'</p></div><div class="card-body"><a href="'+Object.item_data.link+'" class="card-link" target="_blank">'+Object.item_data.link_text+'</a></div></div></div></div><div class="card-footer text-muted">Published on: '+Object.item_published+'</div></div>');
         myPanel.appendTo(myCol);
         myCol.appendTo('#contentPanel');
+      $('.manual').linkify({
+        target: "_blank"
+    });
 }
 /* Data from Twitter card
 item_data.tweet
@@ -48,11 +46,26 @@ item_data.user.username
 item_data.user.avatar
 */
 var twitterCard = function(Object){
-    var myCol = $('<div class="col-lg-4 col-md-16 col-sm-6 col-xs-6 align-items-stretch mt-4 text-center"></div>');
-        var myPanel = $('<div class="card card-outline-info" id="'+Object.item_id+'Panel"><div class="card-block"><img class="rounded-circle" src="images/ali.jpg" alt="Card image cap"><div class="card-title">@'+Object.item_data.user.username+'</div><div class="card-body"><p class="card-text">'+Object.item_data.tweet+'</p></div><div class="card-body"></div></div></div></div></div>');
+    var myCol = $('<div class="col-lg-4 col-md-16 col-sm-6 col-xs-6 align-items-stretch mt-4 text-center twitter"></div>');
+        var myPanel = $('<div class="card card-outline-info" id="'+Object.item_id+'Panel"><div class="card-block"><img class="rounded-circle" src="images/ali.jpg" alt="Card image cap"><div class="card-title">@'+Object.item_data.user.username+'</div><div class="card-body"><p class="card-text">'+Object.item_data.tweet+'</p></div><div class="card-body"></div></div></div></div><div class="card-footer text-muted">Published on: '+Object.item_published+'</div></div>');
         myPanel.appendTo(myCol);
         myCol.appendTo('#contentPanel');
+    $('.twitter').linkify({
+        target: "_blank"
+    });
 }
 /* Data from Instagram card
-
+item_data.user.username
+item_data.image.large
+item_data.tags
+item_data.caption
 */
+var instagramCard = function(Object){
+     var myCol = $('<div class="col-lg-4 col-md-16 col-sm-6 col-xs-6 align-items-stretch mt-4 instagram manual"></div>');
+        var myPanel = $(' <div class="card card-outline-info" id="'+Object.item_id+'Panel"><div class="card-block"><img class="card-img-top" src="images/ali.jpg" alt="Card image cap"><div class="card-title text-center">@'+Object.item_data.user.username+'</div><div class="card-body"><p class="card-text">'+Object.item_data.caption+'</p></div><div class="card-body"></div></div></div>');
+        myPanel.appendTo(myCol);
+        myCol.appendTo('#contentPanel');
+      $('.instagram').linkify({
+        target: "_blank"
+    });
+}
